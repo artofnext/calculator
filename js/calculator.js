56c36803b5c8281f0a5baf4f3fc5850e7018deeb
artofnext <novnovalex@gmail.com>
2019-11-20 15:28:58 +0200

    small clean up

--- js/calculator.js
+++ js/calculator.js
@@ -99,17 +99,6 @@ $(".digit").click(function (event) {
     refreshDisplay();
 })
 
-// $(".debugger").click(function (event) {
-//     $(".operand1").html(calculator.operand1);
-//     $(".operand2").html(calculator.operand2);
-//     $(".deb_operator").text(calculator.operator.toString());
-//     console.log('operator' + calculator.operator);
-//     $(".deb_display").html(calculator.display);
-//     $(".isoperand1").html(calculator.isOperand1.toString());
-//     $(".isoperand2").html(calculator.isOperand2.toString());
-//     $(".isoperator").html(calculator.isOperator.toString());
-//     // console.log("you clicked: ", this.innerText)
-// })
 
 //Log calculator object
 $(".display__container").click(function (event) {
@@ -180,84 +169,3 @@ function calculate() {
 }
 
 
-// class Calculator {
-//     operand1 = 0;
-//     operand2 = 0;
-//     result = 0;
-//     operator = "+";
-//     isOperand1 = false;
-//     isOperand2 = false;
-//     isDot = false;
-//     isOperator = false;
-//     display = 0;
-    
-//     set operand1(num) {
-//         this.operand1 = num;
-//     }
-    
-//     refreshDisplay = function () {
-//         $("#lower_display").html(this.display);
-//     }
-    
-//     clearAll = function () {
-//         this.operand1 = 0;
-//         this.operand2 = 0;
-//         this.result = 0;
-//         this.operator = "+";
-//         this.isOperand1 = false;
-//         this.isDot = false;
-//         this.isOperator = false;
-//         this.display = 0;
-//     }
-    
-//     performCalc = function () {
-//         if (this.isOperand1 && this.isOperand2 && this.isOperator) {
-
-//             switch (this.operator) {
-//                 case "+":
-//                     this.result = this.operand1 + this.operand2;
-//                     break;
-//                 case "-":
-//                     this.result = this.operand1 - this.operand2;
-//                     break;
-//                 case "×":
-//                     this.result = this.operand1 * this.operand2;
-//                     break;
-//                 case "÷":
-//                     this.result = this.operand1 / this.operand2;
-//                     break;
-//                 default:
-//                     console.log('Perform Calc out of operators!');
-//             }
-
-//             //for next operation
-//             this.operand1 = this.result;
-
-//             this.display = this.result;
-
-//         } else {
-//             console.log('Operands or oiperator not set!');
-//         }
-//     }
-
-// }
-
-
-
-//let calculator = new Calculator();
-
-
-//test
-
-// calculator.operand1 = 5;
-// calculator.operand2 = 3;
-// calculator.operator = "+";
-// calculator.isOperand1 = true;
-// calculator.isOperand2 = true;
-// calculator.isOperator = true;
-
-// calculator.performCalc();
-
-// console.log(calculator.operand1)
-
-// calculator.refreshDisplay();
